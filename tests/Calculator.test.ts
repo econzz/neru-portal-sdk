73dd6b1f1bd92af40ef6c4a00047c0d7b3ea6341
Nelson Widjaya <nelson.widjaya@gmail.com>
2020-10-25 16:53:21 +0900

    #9 jasmine install

---
+++ tests/Calculator.test.ts
@@ -0,0 +1,12 @@
+import NHPScript from "../src/NHPScript";
+
+
+describe('initializePlayer', function() {
+  it('generateId', function() {
+    window.NHPScript = new NHPScript();
+
+    window.NHPScript.initialize().then(function(){
+      
+    });
+  });
+});
